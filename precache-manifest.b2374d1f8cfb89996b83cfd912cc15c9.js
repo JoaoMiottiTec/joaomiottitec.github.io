1c3995b56a0035dfdcb1f913b63a676fa1eb9cc0
Joao Vitor dos santos Miotti <103967197+JoaoMiottiTec@users.noreply.github.com>
2022-07-04 13:39:44 -0300

    Add files via upload

---
+++ precache-manifest.b2374d1f8cfb89996b83cfd912cc15c9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "d0800fe89323c995f4ec",
+    "url": "/static/css/main.8f9d28c7.chunk.css"
+  },
+  {
+    "revision": "d0800fe89323c995f4ec",
+    "url": "/static/js/main.d0800fe8.chunk.js"
+  },
+  {
+    "revision": "f6ace53afae71140f9f5",
+    "url": "/static/js/1.f6ace53a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "742e3676e4c6cc6a5e68d7e983d7597f",
+    "url": "/static/media/1.742e3676.svg"
+  },
+  {
+    "revision": "12a7818880bea84ab55261174ffb1c3c",
+    "url": "/static/media/video.12a78188.mp4"
+  },
+  {
+    "revision": "22808bae77c66bd1b33e298ea2f4320e",
+    "url": "/static/media/Icon2.22808bae.jpg"
+  },
+  {
+    "revision": "356aea45f1757b8a8a12cea151933c47",
+    "url": "/static/media/Icon4.356aea45.png"
+  },
+  {
+    "revision": "e4fe4062568d43ebaa8f7e74f229d4e9",
+    "url": "/static/media/Icon7.e4fe4062.jpg"
+  },
+  {
+    "revision": "dd5b452edc62fc9c1fd347266ede0d57",
+    "url": "/static/media/3.dd5b452e.svg"
+  },
+  {
+    "revision": "f29686fb2b67d710aa8196f7dfa1bd5b",
+    "url": "/index.html"
+  }
+];
